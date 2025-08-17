8d2f32dc31e5fb41e8c6d87ec171ac9b2f2f9cfb
eko777e <replitserver445@gmail.com>
2025-08-17 19:58:48 +0400

    Create soyus.py

---
+++ soyus.py
@@ -0,0 +1,12 @@
+bad_words = [
+    "peysər", "Oğras", "Ogras", "Serefsiz", "Şərəfsiz", "anavi",
+    "bacini", "qehbe", "Qəhbə", "varyoxsuz", "Nəslini", "Blə", "Ble", "Blət",
+    "Blet", "sikdir", "sikim", "skm", "pey", "sər", "qələt", "namussuz",
+    "Evindəkidi", "anandı", "Bacindi", "qeyrətsiz", "heyvan", "it oğlu",
+    "qandon", "qeyser", "kisicik", "gehbe", "gəhbə", "ayble", "oğraş",
+    "eşşək", "fahişə", "fayşə", "daşşaq", "Daşaq", "Dassaq",
+    "Dasaq", "sik", "göt", "Dal", "Məməş", "Memeş", "yeyərsən", "sikm",
+    "sikmirem", "sikmirəm", "varyox", "sortunu", "yaşayışını", "oğlanlıq",
+    "qahbe", "vzqrt", "qəhbə", "am", "got", "göt", "pox", "gijdillax", "Pesi",
+    "peyser", "gey", "qehbe", "sikilmis", "sikdir", "sikim", "amciq", "amcıx", "əlinə verim"
+]
