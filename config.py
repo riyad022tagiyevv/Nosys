e7e4d23b88b61888bb794450d38be3a1cba72738
eko777e <replitserver445@gmail.com>
2025-08-17 21:27:09 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-TOKEN = ""
+TOKEN = "8234671504:AAFUi0nvY5pmcUvCVRlzKCgp--j5XsOCXo8"
 LOG_CHANNEL = -1002999709623
-START_IMAGE = ""
-OWNER_URL = "https://t.me/ownerlink"
-SUPPORT_URL = "https://t.me/supportlink"
+START_IMAGE = "https://files.catbox.moe/427fdn.jpg"
+OWNER_URL = "https://t.me/RespublicOwner"
+SUPPORT_URL = "https://t.me/RespublicSupport"
 
 #reponu satan @RespublicOwner üzvlərinə ata desin
