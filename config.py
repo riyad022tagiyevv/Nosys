736ccbe2601bab389a364ecb6c02c5d7b2a7f1fc
eko777e <replitserver445@gmail.com>
2025-08-17 19:58:01 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = ""
+LOG_CHANNEL = -1002999709623
+START_IMAGE = ""
+OWNER_URL = "https://t.me/ownerlink"
+SUPPORT_URL = "https://t.me/supportlink"
+
+#reponu satan @RespublicOwner üzvlərinə ata desin
