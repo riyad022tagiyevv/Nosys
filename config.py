623cbe0e74a0e2299bbda23f2a55cc90d619ff52
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2026-01-12 19:09:17 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = "6140250737:AAGnZZztH3bK0Y9MukyL64QtHzYjomXPeXs"
 LOG_CHANNEL = -1002827565646
-START_IMAGE = "https://www.freepik.com/premium-vector/vector-design-gluten-free-icon-style_186758471.htm#fromView=search&page=1&position=6&uuid=d900babf-0a80-471e-88cb-9a0e07103f49&query=No+soyus"
+START_IMAGE = "https://share.google/9VDnnhnNa5l0FEnIb"
 OWNER_URL = "https://t.me/Axtarmatagyoxdu"
 SUPPORT_URL = "https://t.me/rosexmusic"
 
