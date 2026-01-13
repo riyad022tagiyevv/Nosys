ac01b95bf7400739abcd89e08cdb6cd2776ecc58
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2026-01-13 20:31:55 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = "6140250737:AAGnZZztH3bK0Y9MukyL64QtHzYjomXPeXs"
 LOG_CHANNEL = -1002827565646
-START_IMAGE = "https://i.ibb.co/4mfjbcS/photo-2025-01-16-08-31-35.jpg"
+START_IMAGE = "https://files.catbox.moe/ilzsce.jpg"
 OWNER_URL = "https://t.me/Axtarmatagyoxdu"
 SUPPORT_URL = "https://t.me/rosexmusic"
 
